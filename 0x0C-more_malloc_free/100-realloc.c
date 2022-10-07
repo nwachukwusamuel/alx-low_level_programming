3735e1c93f9138ef08a740ae819380d0e1a093ea
Nwachukwu Samuel <nwachukwusamuel119@gmail.com>
2022-10-07 04:58:55 -0700

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -13,30 +13,41 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *p;
-	unsigned int i, max = new_size;
-	char *oldp = ptr;
+char *ptr1;
+char *old_ptr;
+unsigned int i;
 
-	if (ptr == NULL)
-	{
-		p = malloc(new_size);
-		return (p);
-	}
-	else if (new_size == 0)
-	{
-		free(ptr);
-	}
-	else if (new_size == old_size)
-		return (ptr);
+if (new_size == old_size)
+return (ptr);
 
-	p = malloc(new_size);
-	if (p == NULL)
-		return (NULL);
-	if (new_size > old_size)
-		max = old_size;
-	for (i = 0; i < max; i++)
-		p[i] = oldp[i];
-	free(ptr);
-	return (p);
+if (new_size == 0 && ptr)
+{
+free(ptr);
+return (NULL);
+}
+
+if (!ptr)
+return (malloc(new_size));
+
+ptr1 = malloc(new_size);
+if (!ptr1)
+return (NULL);
+
+old_ptr = ptr;
+
+if (new_size < old_size)
+{
+for (i = 0; i < new_size; i++)
+ptr1[i] = old_ptr[i];
+}
+
+if (new_size > old_size)
+{
+for (i = 0; i < old_size; i++)
+ptr1[i] = old_ptr[i];
+}
+
+free(ptr);
+return(ptr);
 }
 
